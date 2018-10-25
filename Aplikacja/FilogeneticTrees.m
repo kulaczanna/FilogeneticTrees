a4adf675beb88559554612891fff7614aa67bc78
Anna Kułacz <annakulacz@yahoo.com>
2018-10-25 21:36:08 +0200

    GUI - sequence labels, textboxes, checkboxes, matrix table, filogenetic tree axis, add sequence button

--- Aplikacja/FilogeneticTrees.m
+++ Aplikacja/FilogeneticTrees.m
@@ -22,7 +22,7 @@ function varargout = FilogeneticTrees(varargin)
 
 % Edit the above text to modify the response to help FilogeneticTrees
 
-% Last Modified by GUIDE v2.5 04-Oct-2018 18:15:16
+% Last Modified by GUIDE v2.5 25-Oct-2018 21:17:32
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -71,3 +71,376 @@ function varargout = FilogeneticTrees_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+
+
+
+function sequenceEditText1_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText1 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText2_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText2 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText3_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText3 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText4_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText4 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText5_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText5 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText5 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText6_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText6 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText6 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText6_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText7_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText7 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText7 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText7_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText8_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText8 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText8 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText8_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText9_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText9 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText9 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText9_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function sequenceEditText10_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of sequenceEditText10 as text
+%        str2double(get(hObject,'String')) returns contents of sequenceEditText10 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function sequenceEditText10_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sequenceEditText10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in addSequencePushButton.
+function addSequencePushButton_Callback(hObject, eventdata, handles)
+% hObject    handle to addSequencePushButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in checkbox1sequenceCheckBox1.
+function checkbox1sequenceCheckBox1_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox1sequenceCheckBox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1sequenceCheckBox1
+
+
+% --- Executes on button press in sequenceCheckBox2.
+function sequenceCheckBox2_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox2
+
+
+% --- Executes on button press in sequenceCheckBox3.
+function sequenceCheckBox3_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox3
+
+
+% --- Executes on button press in sequenceCheckBox4.
+function sequenceCheckBox4_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox4
+
+
+% --- Executes on button press in sequenceCheckBox5.
+function sequenceCheckBox5_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox5
+
+
+% --- Executes on button press in sequenceCheckBox6.
+function sequenceCheckBox6_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox6
+
+
+% --- Executes on button press in sequenceCheckBox7.
+function sequenceCheckBox7_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox7
+
+
+% --- Executes on button press in sequenceCheckBox8.
+function sequenceCheckBox8_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox8
+
+
+% --- Executes on button press in sequenceCheckBox9.
+function sequenceCheckBox9_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox9
+
+
+% --- Executes on button press in sequenceCheckBox10.
+function sequenceCheckBox10_Callback(hObject, eventdata, handles)
+% hObject    handle to sequenceCheckBox10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of sequenceCheckBox10
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
--- Próbne pliki/probaminimum.m
+++ Próbne pliki/probaminimum.m
@@ -3,92 +3,82 @@ clc
 clear all;
 % load the matrix
 A = [0, 10, 8, 2, 6, 4; 0, 0, 4, 6, 8, 2; 0, 0, 0, 10, 2, 6; 0, 0, 0, 0, 8, 4; 0, 0, 0, 0, 0, 2; 0, 0, 0, 0, 0, 0]
+% A = [0, 8, 9, 7, 4; 0, 0, 4, 8, 2; 0, 0, 0, 2, 6; 0, 0, 0, 0, 2; 0, 0, 0, 0, 0]
+% A = [0, 6, 5, 5; 0, 0, 9, 7; 0, 0, 0, 2; 0, 0, 0, 0]
 % A = [0, 0.4, 0.35, 0.6; 0, 0, 0.45, 0.7; 0, 0, 0, 0.55; 0, 0, 0, 0];
-%A=[0, 0.425, 0.575; 0, 0, 0.7; 0, 0, 0]
-% copy the matrix
+% A=[0, 0.425, 0.575; 0, 0, 0.7; 0, 0, 0]
 
-
-% % find minimum value of the matrix
-% minimumValue = min(A(A>0))
-% % and its index...
-% [minValueY, minValueX] = find(A == minimumValue)
 [sizeY, sizeX] = size(A);
-
 
 for k = 1 : 1 : sizeY - 2
     % find minimum value of the matrix
     minimumValue = min(A(A>0))
     % and its index...
-    [minValueY, minValueX] = find(A == minimumValue)
-    minValueY=minValueY(1)
-    minValueX=minValueX(1)
+    [minY, minX] = find(A == minimumValue)
+     minValueY=min(minY)
+    [Y, X] = find(minY == minValueY)
+     minValueX=minX(Y,X)
     
-    for i = 1 : 1 : sizeY
+%     [minValueY, minValueX] = find(A == minimumValue)
+%     minValueY=minValueY(1)
+%     minValueX=minValueX(1)
+    
+    for i = 1 : 1 : sizeY-1
+        %-------------------------------------
         if(i == 1)
+            
             copyA=A;
             B = zeros(sizeY-1);
-            for j = 1 : 1 : sizeY - 1
-                if(i == j)
-                    B (i, j) = 0
-                elseif(i == minValueY && j == minValueX)
-                    B(1, j) = (copyA(minValueY, j + 1) + copyA(minValueX, j + 1))/2
-                elseif(j > minValueY)
-                        if(j < minValueX)
-                            B(1, j) = (copyA(minValueY, j) + copyA(j, minValueX))/2  
-                        else
-                            B(1, j) = (copyA(minValueY, j+1) + copyA(minValueX, j+1))/2
-                        end
+            
+                for j = 1 : 1 : sizeY - 1
+                
+                    if(i == j)          
+                        B (i, j) = 0   
+                    elseif(i == minValueY && j == minValueX)
+                            B(1, j) = (copyA(minValueY, j + 1) + copyA(minValueX, j + 1))/2
+                    elseif(j > minValueY)
+                            
+                                if(j < minValueX)
+                                    B(1, j) = (copyA(minValueY, j) + copyA(j, minValueX))/2  
+                                else
+                                    B(1, j) = (copyA(minValueY, j+1) + copyA(minValueX, j+1))/2
+                                end
+                                
                     else
+                        
                         if(j < minValueX)
                             B(1, j) = (copyA(j, minValueY) + copyA(j, minValueX))/2
-
                         else
                             B(1, j) = (copyA(j, minValueY) + copyA(minValueX, j))/2
                         end
+                        
                     end
-            end
-        elseif(i >= j)
-            break;
-        elseif(i == minValueY || i == minValueX)
-            B(i, j) = copyA(i + 1, j + 1)
-        elseif(j >= minValueY || j >= minValueX)
-            B(i, j) = copyA(i, j + 1)
+                    
+                end
+                
         else
-            B(i, j) = copyA(i, j)        
+             for j = 1 : 1 : sizeY - 1
+                 
+                if(i == minValueY || i == minValueX)
+                    B(i, j) = copyA(i + 1, j + 1)
+                elseif(i >= j)
+                    B(i, j) = 0
+                elseif( j >= minValueX)
+                    B(i, j) = copyA(i, j+1)
+                else
+                    B(i, j) = copyA(i, j)  
+                end
+                
+             end
+             
         end
+       
     end
+     %------------------------------------
     A = B
     sizeY = sizeY - 1;
+    
 end
 
-% %%
-% clc
-% clear all
-% A = [0, 10, 8, 2, 2, 4; 0, 0, 4, 6, 8, 2; 0, 0, 0, 10, 2, 6; 0, 0, 0, 0, 8, 4; 0, 0, 0, 0, 0, 2; 0, 0, 0, 0, 0, 0]
-% minimumValue = min(A(A>0));
-% [minValueY, minValueX] = find(A == minimumValue)
-%     minY = min(minValueY)
-%     minX = min(minValueX)
-
-
-
-% count length from takson to wezel xD
-% lengthOfBranch = minimumValue/2;
-% % reduce the matrix
-% A = [(copyA(1,1)+copyA(2,2))/2, copyA(1,2); (copyA(3,3) + copyA(3,1))/2, copyA(3,2)]
-% % find minimum of reduced matrix
-% minimumValue = min(A(A>0));
-% % and its index...
-% [minValueY, minValueX] = find(A == minimumValue)
-% % count length from takson to wezel xD
-% lengthOfBranch = minimumValue/2;
-% % reduce reduced matrix...
-% A = [(copyA(3,2)+copyA(3,1)+copyA(3,3))/3]
-% % find minimum of reduced matrix
-% minimumValue = min(A(A>0))
-% % and its index...
-% [minValueY, minValueX] = find(A == minimumValue);
-% % count length from takson to wezel xD
-% lengthOfBranch = minimumValue/2;
 
 
