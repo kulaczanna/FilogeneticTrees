cbb9160ce49ce8c2bbc65ea3541a64feff194665
Anna Kułacz <annakulacz@yahoo.com>
2018-12-06 17:04:53 +0100

    added uitable to GUI

--- Filogenetic Trees/porownywanieSekwencji.m
+++ Filogenetic Trees/porownywanieSekwencji.m
@@ -10,12 +10,14 @@ sequence6 = 'TGGCCCTGTATC';
 
 sequences = [sequence1; sequence2; sequence3; sequence4; sequence5; sequence6]
 [rows, columns] = size(sequences);
+distanceMatrix = zeros(rows)
 
 for i = rows : -1 : 1
     for j = rows : -1 : 1
         if (i ~= j)
             counter = compareSequences(sequences(i, :), sequences(j, :));
             fprintf('sequence %d and %d: %d \n', i, j, counter)
+            distanceMatrix(j, i) = counter;
         end
     end
      rows = rows - 1;
