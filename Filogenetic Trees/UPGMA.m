7c721e44785e4e33d2ce0a12fbd63816027cd0a4
Anna Kułacz <annakulacz@yahoo.com>
2018-12-14 15:01:48 +0100

    new solution with drawing

--- Filogenetic Trees/UPGMA.m
+++ Filogenetic Trees/UPGMA.m
@@ -1,11 +1,10 @@
 clc
 clear all;
 
-distanceMatrix = loadMatrix(5)
+distanceMatrix = loadMatrix(7)
 lengthOfMatrix = length(distanceMatrix);
 clusterGroupsArray = zeros(lengthOfMatrix);
 helperClusterGroupsArray = zeros(2, lengthOfMatrix);
-subNum = 1;
 nodesNumber = 0;
 nodes = [];
 
@@ -19,7 +18,7 @@ for i = 1 : lengthOfMatrix - 1
  
      [minValueY, minValueX] = findFirstMinimumPosition(distanceMatrix);
      [branchLength, minimumValue] = calculateBranchLength(distanceMatrix, minValueY, minValueX);
-     [newClusterGroupsArray, nodesNumber, isMerge] = makeClasterGroupsTest(i, clusterGroupsArray, ...
+     [newClusterGroupsArray, nodesNumber, isMerge, isOneAdded, changedRowNumber] = makeClusterGroupsTest(i, clusterGroupsArray, ...
          helperClusterGroupsArray, minValueY, ...
          minValueX, branchLength, nodesNumber);
      
@@ -30,8 +29,8 @@ for i = 1 : lengthOfMatrix - 1
      newDistanceMatrix = calculateNewDistanceMatrix(lengthOfMatrix, minValueY, minValueX, ...
          distanceMatrixCopy, newDistanceMatrix);       
      
-        nodes = drawTree(subNum, clusterGroupsArray, newClusterGroupsArray, nodesNumber, isMerge, nodes);
-        subNum = subNum + 1;
+        nodes = drawTreeOther(clusterGroupsArray, newClusterGroupsArray, ...
+            nodesNumber, isMerge, nodes, isOneAdded, changedRowNumber);
          
      distanceMatrix = newDistanceMatrix;
      lengthOfMatrix = lengthOfMatrix - 1;
--- Filogenetic Trees/drawTree.m
+++ Filogenetic Trees/drawTree.m
@@ -1,23 +1,25 @@
-function[nodes] = drawTree(subNum, oldClusterGroupsArray, clusterGroupsArray, nodesNumber, isMerge, nodes)
+function[nodes] = drawTree(oldClusterGroupsArray, clusterGroupsArray, nodesNumber, isMerge, nodes, isOneAdded)
   
 a = sum(sum(oldClusterGroupsArray ~= clusterGroupsArray))
+
     if (isMerge)
         nodes(nodes == 0) = max(nodes + 1);
     else
+        
         notEmptyRowsNumber = 0;
         notEmptyRows = [];
         roots = [];
 
-        for j = 1 : length(clusterGroupsArray)
+        for j = 1 : length(clusterGroupsArray)   % licze ile poddrzew po tym ile niepustych wierszy
                 if (~(clusterGroupsArray(j, 1) == 0))
                     notEmptyRowsNumber = notEmptyRowsNumber + 1;
-                    notEmptyRows(1, end + 1) = j;
+                    notEmptyRows(1, end + 1) = j;    % i sprawdzam ktore wiersze sa niepuste
                 end
         end
 
-            for w = 1 : nodesNumber
-                if (~(ismember(w, clusterGroupsArray(notEmptyRows, :))))
-                    roots(1, end + 1) = w;
+            for n = 1 : nodesNumber
+                if (~(ismember(n, clusterGroupsArray(notEmptyRows, :))))
+                    roots(1, end + 1) = n;
 %                     if (length(roots) == notEmptyRowsNumber)
 %                         break;
 %                     end
@@ -45,89 +47,38 @@ a = sum(sum(oldClusterGroupsArray ~= clusterGroupsArray))
     end
     
     for h =  length(nodes) + 1 : nodesNumber
-                nodes(1, h) = 0;
+        nodes(1, h) = 0;
     end
-        nodes
     
-    switch subNum
-        case 1   
-            
-            figure
-            treeplot(nodes);
-            [x,y] = treelayout(nodes);
+    nodes
 
-            for i = 1 : length(x)
-                if (i == length(x) && max(max(clusterGroupsArray)) > i)
-                    text(x(i), y(i), num2str(max(max(clusterGroupsArray))), ...
-                            'VerticalAlignment','top', ...
-                            'HorizontalAlignment','right');
-                else
-                    if(ismember(i, clusterGroupsArray))
-                        text(x(i), y(i), num2str(i), ...
-                        'VerticalAlignment','top', ...
-                        'HorizontalAlignment','right');
-                    end
-                end
-                
-            end
-
-        case 2
-%             nodes2 = [4 6 4 0 6 0];
-            
-            figure
-            treeplot(nodes);
-            [x,y] = treelayout(nodes);
-
-            for i = 1 : length(x)
-                if (ismember(i, clusterGroupsArray))
-                    text(x(i), y(i), num2str(i), ...
-                        'VerticalAlignment','top', ...
-                        'HorizontalAlignment','right');
-                end
-            end
-
-        case 3
-%             nodes3 = [6 7 6 8 7 8 0 0];
-            
-            figure
-            treeplot(nodes);
-            [x,y] = treelayout(nodes);
-
-             for i = 1 : length(x)
-                if (ismember(i, clusterGroupsArray))
-                    text(x(i), y(i), num2str(i), ...
-                        'VerticalAlignment','top', ...
-                        'HorizontalAlignment','right');
-                end
-            end
-
-        case 4
-%             nodes4 = [6 7 6 8 7 8 9 9 0];
-%             nodes = [7 8 7 9 8 10 9 0 10 0]
-            figure
-            treeplot(nodes);
-            [x,y] = treelayout(nodes);
-
-             for i = 1 : length(x)
-                if (ismember(i, clusterGroupsArray))
-                    text(x(i), y(i), num2str(i), ...
-                        'VerticalAlignment','top', ...
-                        'HorizontalAlignment','right');
-                end
-            end
-
-        case 5
-%             nodes5 = [7 8 7 9 8 11 9 10 10 11 0];
-            figure
-            treeplot(nodes);
-            [x,y] = treelayout(nodes);
-
-             for i = 1 : length(x)
-                if (ismember(i, clusterGroupsArray))
-                    text(x(i), y(i), num2str(i), ...
-                        'VerticalAlignment','top', ...
-                        'HorizontalAlignment','right');
-                end
-            end
+    figure
+    treeplot(nodes);
+    [x,y] = treelayout(nodes);
+    for i = 1 : length(x)
+        if (ismember(i, clusterGroupsArray))
+            text(x(i), y(i), num2str(i), ...
+                'VerticalAlignment','top', ...
+                'HorizontalAlignment','right');
+        end
     end
- end
+end
+                 
+%             figure
+%             treeplot(nodes);
+%             [x,y] = treelayout(nodes);
+% 
+%             for i = 1 : length(x)
+%                 if (i == length(x) && max(max(clusterGroupsArray)) > i)
+%                     text(x(i), y(i), num2str(max(max(clusterGroupsArray))), ...
+%                             'VerticalAlignment','top', ...
+%                             'HorizontalAlignment','right');
+%                 else
+%                     if(ismember(i, clusterGroupsArray))
+%                         text(x(i), y(i), num2str(i), ...
+%                         'VerticalAlignment','top', ...
+%                         'HorizontalAlignment','right');
+%                     end
+%                 end
+%             end
+    
