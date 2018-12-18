4ab21dae7f9749249a2d8089e8007e7583d40564
Anna Kułacz <annakulacz@yahoo.com>
2018-12-18 12:25:45 +0100

    calculateParametersToDrawTree is ok

--- Filogenetic Trees/calculateParametersToDrawTree.m
+++ Filogenetic Trees/calculateParametersToDrawTree.m
@@ -1,28 +1,26 @@
-function[nodes, nodesNumbers, leafsToSign, mergeFlag] = drawTree(mergeFlag, ...
-    oldClusterGroupsArray, clusterGroupsArray, nodesNumber, isMerge, nodes, ...
-    changedRowNumber, nodesNumbers, leafsToSign, i)
+function[nodes, nodesNumbers, leafsToSign, mergeFlag] = calculateParametersToDrawTree(i, mergeFlag, ...
+    oldClusterGroupsArray, clusterGroupsArray, nodes, nodesNumber, nodesNumbers, isMerge, changedRowNumber, leafsToSign)
   
     changedPositionsInClustersNumber = sum(sum(oldClusterGroupsArray ~= clusterGroupsArray));
     oldNodesNumbers = nodesNumbers;
     
-    for x = (length(nodes) + 1) : nodesNumber
-        nodesNumbers(changedRowNumber, end + 1) = x;
+    for k = (length(nodes) + 1) : nodesNumber
+        nodesNumbers(changedRowNumber, end + 1) = k;
     end
         
     if (isMerge)
-        
        helperZerosVector = oldNodesNumbers(changedRowNumber, :);
        helperVectorWithoutZeros = helperZerosVector(helperZerosVector ~= 0);
        vectorToChangeNode = nodes(helperVectorWithoutZeros);
-       zeroPosition = helperVectorWithoutZeros(find(vectorToChangeNode == 0));
+       zeroPosition = helperVectorWithoutZeros(vectorToChangeNode == 0);
        nodes(zeroPosition) = max(nodes + 1);
        mergeFlag = mergeFlag + 1;
         
     elseif (changedPositionsInClustersNumber == 2)
-        
-        for c = (length(nodes) + 1) : nodesNumber - 1
-            nodes(1, c) = nodesNumber;
+        for j = (length(nodes) + 1) : nodesNumber - 1
+            nodes(1, j) = nodesNumber;
         end
+        
         if (i == 1)
             leafsToSign = [1, 2];
         else
@@ -32,23 +30,25 @@ function[nodes, nodesNumbers, leafsToSign, mergeFlag] = drawTree(mergeFlag, ...
         end
          
     elseif (changedPositionsInClustersNumber == 1)
-        
        helperZerosVector = oldNodesNumbers(changedRowNumber, :);
        helperVectorWithoutZeros = helperZerosVector(helperZerosVector ~= 0);
        vectorToChangeNode = nodes(helperVectorWithoutZeros);
-       zeroPosition = helperVectorWithoutZeros(find(vectorToChangeNode == 0));
+       zeroPosition = helperVectorWithoutZeros(vectorToChangeNode == 0);
+       
        if (length(zeroPosition) > 1)
            zeroPosition = zeroPosition(changedRowNumber);
        end
+       
        newNodeNumber = max(nodes) + 2;
        nodes(1, zeroPosition) = newNodeNumber;
        nodes(1, length(nodes) + 1) = newNodeNumber;
        leafsToSign(1, end + 1) = max(leafsToSign) + 2 + mergeFlag;
-       mergeFlag = 0;        
+       mergeFlag = 0; 
+       
     end
 
-     for h =  length(nodes) + 1 : nodesNumber
-         nodes(1, h) = 0;
+     for l =  length(nodes) + 1 : nodesNumber
+         nodes(1, l) = 0;
      end
     
 end
